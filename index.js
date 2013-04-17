8a5f535c0e762c9afee3ae8eda2643cb1493a12d
albertyfwu <albertyfwu@gmail.com>
2013-04-17 02:07:15 -0400

    changed prototype inheritance to be consistent with engine.io; added javadocs

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
-var NormalSocket = require('../engine.io/lib/socket');
+/**
+ * Module requirements.
+ */
+
+var Socket = require('../engine.io/lib/socket');
+
+/**
+ * Exports the constructor.
+ */
 
 module.exports = ReliableSocket;
 
+/**
+ * Reliable socket constructor.
+ *
+ * @api public.
+ */
+
 function ReliableSocket(id, server, transport) {
-  NormalSocket.call(this, id, server, transport);
+  Socket.call(this, id, server, transport);
 }
 
-ReliableSocket.prototype = NormalSocket.prototype;
+/**
+ * Inherits from Socket.
+ *
+ * @api public.
+ */
 
+ReliableSocket.prototype.__proto__ = Socket.prototype;
+
+/**
+ * Overrides onClose to implement reliability layer.
+ *
+ * @api private
+ */
+
+ReliableSocket.prototype.onClose = function(reason, description) {
+	Socket.prototype.onClose.call(this, reason, description);
+}
